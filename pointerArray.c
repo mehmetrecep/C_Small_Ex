4ed3e2a02790d9317d1445ddbc80db22894fefd4
mehmetrecep <73506878+mehmetrecep@users.noreply.github.com>
2023-05-10 00:42:18 +0000

    changing the value by changing the value the adress or pointer refer to.

--- pointerArray.c
+++ pointerArray.c
@@ -4,9 +4,18 @@ int main(void){
     int arr[3] = {15,16,17};
 
     // To show the array element with its address
+    printf("before changing the values of array: \n");
     for(int i = 0; i < 3; i++){
-        printf("%d ",*arr+i);
+        printf("%d : ",*arr+i);
         printf("%p\n",arr+i);
     }
+    printf("\n");
+    printf("After changing the values of array: \n");
+    for(int i = 0; i < 3; i++){
+        * (arr+i) = 18 + i;
+        printf("%d : ",*arr+i);
+        printf("%p\n",arr+i);
+    }
+    
     return 0;
 }
