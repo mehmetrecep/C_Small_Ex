41943ef1468dacc0758edc5e7061a7f829f7d6c0
mehmetrecep <73506878+mehmetrecep@users.noreply.github.com>
2023-05-13 21:28:00 +0000

    Link List

---
+++ linkStructuresTogetherInAlinkedList.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct point{
+    int x;
+    int y;
+    struct point *next;
+};
+
+int main(void){
+    struct point pt1 = {1,2, NULL};
+    struct point pt2 = {-2,3, NULL};
+    struct point pt3 = {5, -4, NULL};
+    struct point *start, *ptr;
+    start = &pt1;
+    pt1.next = &pt2;
+    pt2.next = &pt3;
+
+    /*
+    printf("pt1 address :%p\n",&pt1);
+    printf("%p\n",pt1.next);
+    printf("pt2 address :%p\n",&pt2);
+    printf("%p\n",pt2.next);
+    */
+    ptr = start;
+   while(ptr != NULL){
+    printf("(%d, %d)\n", ptr->x, ptr->y);
+    printf("%p\n", ptr);
+    ptr = ptr->next;
+    }
+
+    return 0;
+}
